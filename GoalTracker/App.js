05b3ff473aad354bf1b178101054f12bb2940a8a
ashish-pro <ashishbit017@outlook.com>
2024-01-15 17:12:41 +0530

    'native-revision-gtacker'

---
+++ GoalTracker/App.js
@@ -0,0 +1,29 @@
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder='Enter your goal ' style={styles.textInput} />
+        <Button title='Add Goals' />
+      </View>
+      <View>
+        <Text>List of Goals...</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  appContainer: {
+    padding: 30,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+  },
+});
